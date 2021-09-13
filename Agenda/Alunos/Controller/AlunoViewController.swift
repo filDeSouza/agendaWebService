6043fefe3d84d83cc16474cf332fd2e1f0a3332e
Filipe <fil.desouza@gmail.com>
2021-09-13 16:01:59 -0300

    segunda atualização

--- Agenda/Alunos/Controller/AlunoViewController.swift
+++ Agenda/Alunos/Controller/AlunoViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import CoreData
 
 class AlunoViewController: UIViewController, ImagePickerFotoSelecionada {
     
@@ -26,10 +25,6 @@ class AlunoViewController: UIViewController, ImagePickerFotoSelecionada {
     
     // MARK: - Atributos
     
-    var contexto:NSManagedObjectContext {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        return appDelegate.persistentContainer.viewContext
-    }
     let imagePicker = ImagePicker()
     var aluno:Aluno?
     
@@ -78,6 +73,36 @@ class AlunoViewController: UIViewController, ImagePickerFotoSelecionada {
         self.present(multimidia, animated: true, completion: nil)
     }
     
+    func montaDicionarioDeParametros() -> Dictionary<String, String> {
+        
+        var id = ""
+        
+        if aluno?.id == nil {
+            id = String(describing: UUID())
+        }
+        else {
+            guard let idDoAlunoExistente = aluno?.id else { return [:] }
+            id = String(describing: idDoAlunoExistente)
+        }
+        
+        guard let nome = textFieldNome.text else { return [:] }
+        guard let endereco = textFieldEndereco.text else { return [:] }
+        guard let telefone = textFieldTelefone.text else { return [:] }
+        guard let site = textFieldSite.text else { return [:] }
+        guard let nota = textFieldNota.text else { return [:] }
+        
+        let dicionario:Dictionary<String, String> = [
+            "id" : id.lowercased(),
+            "nome" : nome,
+            "endereco" : endereco,
+            "telefone" : telefone,
+            "site" : site,
+            "nota" : nota
+        ]
+        
+        return dicionario
+    }
+    
     // MARK: - Delegate
     
     func imagePickerFotoSelecionada(_ foto: UIImage) {
@@ -99,22 +124,17 @@ class AlunoViewController: UIViewController, ImagePickerFotoSelecionada {
     }
     
     @IBAction func buttonSalvar(_ sender: UIButton) {
-        if aluno == nil {
-            aluno = Aluno(context: contexto)
-        }
-        aluno?.nome = textFieldNome.text
-        aluno?.endereco = textFieldEndereco.text
-        aluno?.telefone = textFieldTelefone.text
-        aluno?.site = textFieldSite.text
-        aluno?.nota = (textFieldNota.text! as NSString).doubleValue
-        aluno?.foto = imageAluno.image
-        
-        do {
-            try contexto.save()
-            navigationController?.popViewController(animated: true)
-        } catch {
-            print(error.localizedDescription)
-        }
+        let json = montaDicionarioDeParametros()
+        Repositorio().salvaAluno(aluno: json)
+        navigationController?.popViewController(animated: true)
     }
     
 }
+
+
+
+
+
+
+
+
--- Agenda/Home/Controller/HomeTableViewController.swift
+++ Agenda/Home/Controller/HomeTableViewController.swift
@@ -7,28 +7,24 @@
 //
 
 import UIKit
-import CoreData
-import SafariServices
 
-class HomeTableViewController: UITableViewController, UISearchBarDelegate, NSFetchedResultsControllerDelegate {
+class HomeTableViewController: UITableViewController, UISearchBarDelegate {
     
     //MARK: - Variáveis
     
-    var contexto:NSManagedObjectContext {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        return appDelegate.persistentContainer.viewContext
-    }
     let searchController = UISearchController(searchResultsController: nil)
-    var gerenciadorDeResultados:NSFetchedResultsController<Aluno>?
     var alunoViewController:AlunoViewController?
-    var mensagem = Mensagem()
+    var alunos:Array<Aluno> = []
     
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configuraSearch()
-        self.recuperaAluno()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        recuperaAlunos()
     }
     
     // MARK: - Métodos
@@ -39,110 +35,39 @@ class HomeTableViewController: UITableViewController, UISearchBarDelegate, NSFet
         }
     }
     
+    func recuperaAlunos() {
+        Repositorio().recuperaAlunos { (listaDeAlunos) in
+            self.alunos = listaDeAlunos
+            self.tableView.reloadData()
+        }
+    }
+    
     func configuraSearch() {
         self.searchController.searchBar.delegate = self
         self.searchController.dimsBackgroundDuringPresentation = false
         self.navigationItem.searchController = searchController
     }
     
-    func recuperaAluno(filtro:String = "") {
-        let pesquisaAluno:NSFetchRequest<Aluno> = Aluno.fetchRequest()
-        let ordenaPorNome = NSSortDescriptor(key: "nome", ascending: true)
-        pesquisaAluno.sortDescriptors = [ordenaPorNome]
-        
-        if verificaFiltro(filtro) {
-            pesquisaAluno.predicate = filtraAluno(filtro)
-        }
-        
-        gerenciadorDeResultados = NSFetchedResultsController(fetchRequest: pesquisaAluno, managedObjectContext: contexto, sectionNameKeyPath: nil, cacheName: nil)
-        gerenciadorDeResultados?.delegate = self
-        
-        do {
-            try gerenciadorDeResultados?.performFetch()
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
-    
-    func filtraAluno(_ filtro:String) -> NSPredicate {
-        return NSPredicate(format: "nome CONTAINS %@", filtro)
-    }
-    
-    func verificaFiltro(_ filtro:String) -> Bool {
-        if filtro.isEmpty {
-            return false
-        }
-        return true
-    }
-    
     @objc func abrirActionSheet(_ longPress:UILongPressGestureRecognizer) {
         if longPress.state == .began {
-            guard let alunoSelecionado = gerenciadorDeResultados?.fetchedObjects?[(longPress.view?.tag)!] else { return }
-            let menu = MenuOpcoesAlunos().configuraMenuDeOpcoesDoAluno(completion: { (opcao) in
-                switch opcao {
-                case .sms:
-                    if let componenteMensagem = self.mensagem.configuraSMS(alunoSelecionado) {
-                        componenteMensagem.messageComposeDelegate = self.mensagem
-                        self.present(componenteMensagem, animated: true, completion: nil)
-                    }
-                    break
-                case .ligacao:
-                    guard let numeroDoAluno = alunoSelecionado.telefone else { return }
-                    if let url = URL(string: "tel://\(numeroDoAluno)"), UIApplication.shared.canOpenURL(url) {
-                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                    }
-                    break
-                case .waze:
-                    if UIApplication.shared.canOpenURL(URL(string: "waze://")!) {
-                        guard let enderecoDoAluno = alunoSelecionado.endereco else { return }
-                        Localizacao().converteEnderecoEmCoordenadas(endereco: enderecoDoAluno, local: { (localizacaoEncontrada) in
-                            let latitude = String(describing: localizacaoEncontrada.location!.coordinate.latitude)
-                            let longitude = String(describing: localizacaoEncontrada.location!.coordinate.longitude)
-                            let url:String = "waze://?ll=\(latitude),\(longitude)&navigate=yes"
-                            UIApplication.shared.open(URL(string: url)!, options: [:], completionHandler: nil)
-                        })
-                    }
-                    break
-                case .mapa:
-                    let mapa = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "mapa") as! MapaViewController
-                    mapa.aluno = alunoSelecionado
-                    
-                    self.navigationController?.pushViewController(mapa, animated: true)
-                    break
-                case .abrirPaginaWeb:
-                    
-                    if let urlDoAluno = alunoSelecionado.site {
-                        
-                        var urlFormatada = urlDoAluno
-                        
-                        if !urlFormatada.hasPrefix("http://") {
-                            urlFormatada = String(format: "http://%@", urlFormatada)
-                        }
-                        
-                        guard let url = URL(string: urlFormatada) else { return }
-                        let safariViewController = SFSafariViewController(url: url)
-                        self.present(safariViewController, animated: true, completion: nil)
-                    }
-                    
-                    break
-                }
-            })
-            self.present(menu, animated: true, completion: nil)
+            let alunoSelecionado = alunos[(longPress.view?.tag)!]
+            guard let navigation = navigationController else { return }
+            let menu = MenuOpcoesAlunos().configuraMenuDeOpcoesDoAluno(navigation: navigation, alunoSelecionado: alunoSelecionado)
+            present(menu, animated: true, completion: nil)
         }
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let contadorListaDeAlunos = gerenciadorDeResultados?.fetchedObjects?.count else { return 0 }
-        return contadorListaDeAlunos
+        return alunos.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let celula = tableView.dequeueReusableCell(withIdentifier: "celula-aluno", for: indexPath) as! HomeTableViewCell
         celula.tag = indexPath.row
         let longPress = UILongPressGestureRecognizer(target: self, action: #selector(abrirActionSheet(_:)))
-        guard let aluno = gerenciadorDeResultados?.fetchedObjects![indexPath.row] else { return celula }
+        let aluno = alunos[indexPath.row]
         celula.configuraCelula(aluno)
         celula.addGestureRecognizer(longPress)
         
@@ -158,14 +83,10 @@ class HomeTableViewController: UITableViewController, UISearchBarDelegate, NSFet
             AutenticacaoLocal().autorizaUsuario(completion: { (autenticado) in
                 if autenticado {
                     DispatchQueue.main.async {
-                        guard let alunoSelecionado = self.gerenciadorDeResultados?.fetchedObjects![indexPath.row] else { return }
-                        self.contexto.delete(alunoSelecionado)
-                        
-                        do {
-                            try self.contexto.save()
-                        } catch {
-                            print(error.localizedDescription)
-                        }
+                        let alunoSelecionado = self.alunos[indexPath.row]
+                        Repositorio().deletaAluno(aluno: alunoSelecionado)
+                        self.alunos.remove(at: indexPath.row)
+                        self.tableView.deleteRows(at: [indexPath], with: .fade)
                     }
                 }
             })
@@ -176,26 +97,12 @@ class HomeTableViewController: UITableViewController, UISearchBarDelegate, NSFet
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let alunoSelecionado = gerenciadorDeResultados?.fetchedObjects![indexPath.row] else { return }
+        let alunoSelecionado = alunos[indexPath.row]
         alunoViewController?.aluno = alunoSelecionado
     }
     
-    // MARK: - FetchedResultsControllerDelegate
-    
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        switch type {
-        case .delete:
-            guard let indexPath = indexPath else { return }
-            tableView.deleteRows(at: [indexPath], with: .fade)
-            break
-        default:
-            tableView.reloadData()
-        }
-    }
-    
     @IBAction func buttonCalculaMedia(_ sender: UIBarButtonItem) {
-        guard let listaDeAlunos = gerenciadorDeResultados?.fetchedObjects else { return }
-        CalculaMediaAPI().calculaMediaGeralDosAlunos(alunos: listaDeAlunos, sucesso: { (dicionario) in
+        CalculaMediaAPI().calculaMediaGeralDosAlunos(alunos: alunos, sucesso: { (dicionario) in
             if let alerta = Notificacoes().exibeNotificacaoDeMediaDosAlunos(dicionarioDeMedia: dicionario) {
                 self.present(alerta, animated: true, completion: nil)
             }
@@ -212,13 +119,14 @@ class HomeTableViewController: UITableViewController, UISearchBarDelegate, NSFet
     // MARK: - SearchBarDelegate
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard let nomeDoAluno = searchBar.text else { return }
-        recuperaAluno(filtro: nomeDoAluno)
+        if let texto = searchBar.text {
+            alunos = Filtro().filtraAlunos(listaDeAlunos: alunos, texto: texto)
+        }
         tableView.reloadData()
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        recuperaAluno()
+        alunos = AlunoDAO().recuperaAlunos()
         tableView.reloadData()
     }
     
